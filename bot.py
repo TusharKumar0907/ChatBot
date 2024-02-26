367003e8e805e971de7082966888cdab484caada
TUSHAR <tktusharkumar12345@gmail.com>
2024-02-26 21:14:37 +0530

    added more thing

--- bot.py
+++ bot.py
@@ -4,7 +4,6 @@ from docs import clean_corpus
 
 CORPUS_FILE = "./data"
 
-
 chatbot = ChatBot("Chatpot")
 trainer = ListTrainer(chatbot)
 cleaned_corpus = clean_corpus(CORPUS_FILE)
@@ -13,12 +12,16 @@ trainer.train(cleaned_corpus)
 
 def get_responses_until_word(temp, user_input):
     word = user_input
+    next = "next"
     while True:
         response = chatbot.get_response(word)
         word = response.text
         if temp in response.text:  
             break
-        print(response)
+        if next in response.text:
+            print("")
+        else:
+            print(response)
 
 
 exit_conditions = (":q", "quit", "exit")
