88a5823af5de96d30e2fde71b05af42dccfed2d0
TUSHAR <tktusharkumar12345@gmail.com>
2024-02-26 18:44:36 +0530

    Cleaned data file

--- bot.py
+++ bot.py
@@ -1,12 +1,30 @@
 from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+from docs import clean_corpus
+
+CORPUS_FILE = "./data"
 
 
 chatbot = ChatBot("Chatpot")
+trainer = ListTrainer(chatbot)
+cleaned_corpus = clean_corpus(CORPUS_FILE)
+trainer.train(cleaned_corpus)
+
+
+def get_responses_until_word(temp, user_input):
+    word = user_input
+    while True:
+        response = chatbot.get_response(word)
+        word = response.text
+        if temp in response.text:  
+            break
+        print(response)
+
 
 exit_conditions = (":q", "quit", "exit")
 while True:
-    query = input("> ")
+    query = input("")
     if query in exit_conditions:
         break
     else:
-        print(f"🪴 {chatbot.get_response(query)}")
+        get_responses_until_word("STOP", query.lower())
