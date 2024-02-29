0c1287cdc7092851a299c5d3a18fb363da712915
TusharKumar0907 <100371860+TusharKumar0907@users.noreply.github.com>
2024-02-29 17:31:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 from docs import clean_corpus
-
+from tabula.io import read_pdf
 CORPUS_FILE = "./data"
+PDFPATH = './brochure.pdf'
 
 chatbot = ChatBot("Chatpot")
 trainer = ListTrainer(chatbot)
@@ -30,4 +31,17 @@ while True:
     if query in exit_conditions:
         break
     else:
-        get_responses_until_word("STOP", query.lower())
+        if query.lower() == "important contacts of university":
+            dfs = read_pdf(PDFPATH, pages='10', encoding='cp1252')
+            print(dfs[0])
+        if query.lower() == "hostel charges":
+            dfs = read_pdf(PDFPATH, pages='29', encoding='cp1252')
+            print(dfs[0])
+        if query.lower() == "documents required for admission":
+            dfs = read_pdf(PDFPATH, pages='36', encoding='cp1252')
+            print(dfs[0])
+        if query.lower() == "minor degree":
+            dfs = read_pdf(PDFPATH, pages='57', encoding='cp1252')
+            print(dfs[0])        
+        else:
+            get_responses_until_word("STOP", query.lower())
